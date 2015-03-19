d018d997fe7faad76852cebc8acb32e7fc1dd971
Eddie Jimenez <casadebanho@gmail.com>
2015-03-19 00:34:04 -0700

    playing with labeling the ships

--- Assets/scripts/PlayerController.js
+++ Assets/scripts/PlayerController.js
@@ -3,6 +3,13 @@
 var health: float;
 var moveSpeed: float;
 var pos: float;
+var photonView: PhotonView;
+var nameText: UI.Text;
+
+function Start() {
+	Debug.Log("player" + nameText.text);
+	photonView = GetComponent(PhotonView);
+}
 
 function OnPhotonSerializeView(stream: PhotonStream, info: PhotonMessageInfo) {
 	if(stream.isWriting == true) {
@@ -14,7 +21,6 @@ function OnPhotonSerializeView(stream: PhotonStream, info: PhotonMessageInfo) {
 }
 
 function Update() {
-	Debug.Log("mine" + PhotonView.isMine);
 	var move = Input.GetAxis("Horizontal");
 	transform.position.x += move * moveSpeed * Time.deltaTime;
 	pos = 1;
