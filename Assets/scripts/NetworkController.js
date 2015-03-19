99655dd6831989424967dad91bd8cd0edfa0a645
Eddie Jimenez <casadebanho@gmail.com>
2015-03-18 18:06:50 -0700

    initial commit

---
+++ Assets/scripts/NetworkController.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+function Start() {
+	PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	PhotonNetwork.ConnectUsingSettings("0.1");
+}
+
+function OnGUI() {
+	GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+}
+
+function OnJoinedLobby() {
+	PhotonNetwork.JoinRandomRoom();
+}
+
+function OnPhotonRandomJoinFailed() {
+	PhotonNetwork.CreateRoom(null);
+}
+
+function OnCreatedRoom() {
+	Debug.Log("Room name: " + PhotonNetwork.room.name);
+}
+
+function OnJoinedRoom() {
+	PhotonNetwork.Instantiate("player", Vector3(0,0,0), Quaternion.identity, 0);
+}
---
+++ Assets/scripts/PlayerController.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var health: float;
+var moveSpeed: float;
+var pos: float;
+
+function OnPhotonSerializeView(stream: PhotonStream, info: PhotonMessageInfo) {
+	if(stream.isWriting == true) {
+		stream.SendNext(health);
+	}
+	else {
+		health = stream.ReceiveNext();
+	}
+}
+
+function Update() {
+	Debug.Log("mine" + PhotonView.isMine);
+	var move = Input.GetAxis("Horizontal");
+	transform.position.x += move * moveSpeed * Time.deltaTime;
+	pos = 1;
+}
