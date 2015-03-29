81a1e722276836537a1d80cde82bc36917683fac
Eddie Jimenez <casadebanho@gmail.com>
2015-03-29 02:23:25 -0700

    added lobby and working on networking

--- Assets/scripts/LobbyController.js
+++ Assets/scripts/LobbyController.js
@@ -1,16 +1,47 @@
 ﻿#pragma strict
 
-private var start: GameObject;
+private var ns: NetworkController4;
+private var status: UI.Text;
+private var roomList: UI.Text;
+private var playerName: UI.Text;
+private var roomName: UI.Text;
+private var createButton: UI.Button;
+private var joinButton: UI.Button;
 
-function Start () {
-//	blah = 
-	
-	
-	for (var child in transform){
-		Debug.Log("child" + child);
-	}
-	
-	start = GameObject.Find("StartButton");
-	
-	Debug.Log("start" + start);
+function Start() {
+  ns = GameObject.Find("NetworkManager").GetComponent.<NetworkController4>();
+  status = GameObject.Find("Status").GetComponent.<UI.Text>();
+  roomList = GameObject.Find("RoomList").GetComponent.<UI.Text>();
+  playerName = GameObject.Find("PlayerName").Find("Text").GetComponent.<UI.Text>();
+  roomName = GameObject.Find("NewGameName").Find("Text").GetComponent.<UI.Text>();
+  createButton = GameObject.Find("CreateButton").GetComponent.<UI.Button>();
+  joinButton = GameObject.Find("JoinButton").GetComponent.<UI.Button>();
+}
+
+function Update() {
+  if(roomName.text) {
+    createButton.interactable = true;
+  }
+  else {
+    createButton.interactable = false;
+  }
+
+  if(!ns.inLobby) {
+    status.text = "Logging in...";
+    createButton.interactable = false;
+  }  
+  else if(ns.inLobby && !ns.roomListComplete) {
+    status.text = "Logged in. Finding rooms...";
+  }
+  else if(ns.inLobby && ns.roomListComplete) {
+    if(ns.roomList.length == 0) {
+      status.text = "Logged in. No rooms found";
+      joinButton.interactable = false;
+    }
+    else {
+      status.text = "Logged in";
+      roomList.text = "<rooms listed here>";
+      joinButton.interactable = true;
+    }
+  }
 }
---
+++ Assets/scripts/Network/NetworkController4.js
@@ -0,0 +1,114 @@
+﻿#pragma strict
+
+var roomNameOld = "space";
+var roomList: RoomInfo[];
+private var VERSION = "v0.0.1";
+
+var inLobby: boolean = false;
+var roomListComplete: boolean = false;
+private var roomName: String;
+private var namez: String;
+
+function Start() {
+	PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	PhotonNetwork.ConnectUsingSettings(VERSION);
+}
+
+function OnGUI() {
+	GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+}
+
+function OnJoinedLobby() {
+	inLobby = true;
+}
+
+function OnReceivedRoomListUpdate() {
+	roomList = PhotonNetwork.GetRoomList();
+	roomListComplete = true;
+}
+
+function CreateNewRoom() {
+	roomName = GameObject.Find("NewGameName").Find("Text").GetComponent.<UI.Text>().text;
+	PhotonNetwork.CreateRoom(roomName);
+}
+
+function OnJoinedRoom() {
+//	namez = PhotonNetwork.RoomInfo.name;
+//	Debug.Log("namez " + namez);
+	
+	// var list = PhotonNetwork.GetRoomList();
+	// Debug.Log("rooms " + list.length);
+
+	
+}
+
+
+/*
+function OnPhotonSerializeView(stream: PhotonStream, info: PhotonMessageInfo) {
+	if(stream.isWriting == true) {
+		stream.SendNext(joinGame);
+	}
+	else {
+		Debug.Log("joined " + stream.ReceiveNext());	
+	}
+}
+
+function OnReceivedRoomListUpdate() {
+	roomList = PhotonNetwork.GetRoomList();
+
+	if(roomList.length == 0) {
+		GameReady();
+	}
+	else if(roomList.length != 0) {
+		GameInProgress();
+	}
+}
+
+function GameInProgress() {
+	if(roomList.length == 0) {
+		GameReady();
+	}
+	else {
+		status.text = "Game in session. Please wait...";
+		joinButton.GetComponent(UI.Button).interactable = false;
+		
+		joinButton.SetActive(true);
+		cancelButton.SetActive(false);
+		startButton.SetActive(false);
+	}
+}
+
+function GameReady() {
+	joinGame = false;
+	nameField.enabled = true;
+
+	if(roomList && roomList.length != 0) {
+		GameInProgress();
+	}
+	else {
+		status.text = "Ready to play";
+		joinButton.GetComponent(UI.Button).interactable = true;
+		
+		joinButton.SetActive(true);
+		cancelButton.SetActive(false);
+		startButton.SetActive(false);
+	}
+}
+
+function StartGame() {
+	if(!nameField.text) {
+		GameReady();
+	}
+	else {
+		nameField.enabled = false;
+		status.text = "Joined. Click Start or Cancel";
+		joinButton.SetActive(false);
+		cancelButton.SetActive(true);
+		startButton.SetActive(true);
+		
+		joinGame = true;
+		
+//		Debug.Log("player " + PhotonNetwork.playerList[0] + " name " + nameField.text);
+	}
+}
+*/
