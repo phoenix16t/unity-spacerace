811e43377a9bb8b9211888e16574be636000dbbd
Eddie Jimenez <casadebanho@gmail.com>
2015-03-30 10:08:49 -0700

    network changes that were somehow lost

---
+++ Assets/scripts/Network/NetworkController3.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var VERSION = "v0.0.1";
+private var photonView: PhotonView;
+
+function Start() {
+	photonView = GetComponent(PhotonView);
+	PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	PhotonNetwork.ConnectUsingSettings(VERSION);
+}
+
+function OnGUI() {
+	GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+}
+
+function OnPhotonSerializeView(stream: PhotonStream, info: PhotonMessageInfo) {
+	if(stream.isWriting == true) {
+		// stream.SendNext(joinGame);
+	}
+	else {
+		// tempJoined = stream.ReceiveNext();
+	}
+}
+
+function OnJoinedLobby() {
+	PhotonNetwork.Instantiate("lobbyCanvas", Vector3.zero, Quaternion.identity, 0);
+}
