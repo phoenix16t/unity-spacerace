9fa5f8178b51d29a169616dba0b3dcd02180a39d
Eddie Jimenez <casadebanho@gmail.com>
2015-05-03 21:57:53 -0700

    improving player controls, implementing parent game objects

--- Assets/scripts/PlayerController.js
+++ Assets/scripts/PlayerController.js
@@ -3,39 +3,29 @@
 var moveSpeed: float;
 var topBoundary: float;
 var bottomBoundary: float;
-var fireRate: float;
 var Explosion: GameObject;
 private var distance: float;
-private var nextShot: float;
+private var mousePos : Vector2;
+private var screenPos : Vector3;
 
 function Start() {
-	distance = Vector3.Distance(Vector3.zero, Camera.main.transform.position);
+	distance = transform.position.z - camera.main.transform.position.z;
 }
 
 function Update() {
-	// determine mouse position
-	var mousePos = Input.mousePosition;
-	if(mousePos.y > topBoundary) { mousePos.y = topBoundary; }
-	else if(mousePos.y < bottomBoundary) { mousePos.y = bottomBoundary; };	
-	mousePos.z = distance;
-	mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-	mousePos.x = 0;
-	
-	// set ship rotation angle
-	transform.LookAt(mousePos);
+	// initialize
+	mousePos = Input.mousePosition;
+	if(mousePos.y > 560) { mousePos.y = 560; }
+	else if(mousePos.y < 40) { mousePos.y = 40; }
+
+	// convert mouse position to screen position
+	screenPos = camera.main.ScreenToWorldPoint(Vector3(mousePos.x, mousePos.y, distance));
+	screenPos.x = 0;
+
+	// set rotation
+	transform.rotation.eulerAngles.z = Mathf.Atan2((screenPos.y - transform.position.y), (screenPos.x - transform.position.x)) * Mathf.Rad2Deg;
 
 	// move ship
-	transform.position = Vector3.MoveTowards(transform.position, mousePos, moveSpeed);
+	transform.position = Vector3.MoveTowards(transform.position, screenPos, moveSpeed);
 	transform.position.x = -7;
-
-	// shoot
-	// if(Input.GetButton("Fire1") && Time.time > nextShot) {
-	// 	Debug.Log("bang");
-	// 	nextShot = Time.time + fireRate;
-	// }
-
-
-	// throw away - players coming in late to current round should watch
-	// t += Time.deltaTime / 5;
-	// gameObject.renderer.material.color = Color.Lerp(Color.green, Color.white, t);
 }
